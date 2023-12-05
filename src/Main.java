72f412c0fa25d3cb2c654ab509fb9c5b23101c8c
William <Williamle@hotmail.dk>
2023-12-05 18:12:39 +0100

    The itumulator have been updated Week 2 and 3 inputfiles have been added fileReader have been updated to fit all the types of lines from week 1 and 2

--- src/Main.java
+++ src/Main.java
@@ -26,19 +26,25 @@ public class Main {
         World world = p.getWorld();
 
         Location place = new Location(0, 1);
-        Location bearSpawn = new Location(3,3);
         Person person = new Person(place);
-        Bear bear = new Bear(0, 10, world);
 
         world.setTile(place, person);
-        world.setTile(bearSpawn, bear);
 
         //Adds the inputlist from the filereader into main
         for(ArrayList<String> spawnObject : file.inputLines){
             int min = Integer.parseInt(spawnObject.get(1));
+
             int max = Integer.parseInt(spawnObject.get(2));
+
             int spawnObjectAmount = ThreadLocalRandom.current().nextInt(min,max + 1);
-            addObject(spawnObject.get(0),world,spawnObjectAmount);
+
+            if (spawnObject.size() > 3){
+                Location objectStartSpawn = new Location(Integer.parseInt(spawnObject.get(3)), Integer.parseInt(spawnObject.get(4)));
+                System.out.println(objectStartSpawn);
+                addObject(spawnObject.get(0),world,spawnObjectAmount, objectStartSpawn);
+            }else{
+                addObject(spawnObject.get(0),world,spawnObjectAmount, null);
+            }
         }
 
         DisplayInformation di = new DisplayInformation(Color.RED,"steve",true);
@@ -63,20 +69,34 @@ public class Main {
 
     //Method for spawning object on random tiles. Cheecks it spawns on empty tiles
     //Uses a switch case system that spawns the specific object based on the string thats asigned.
-    public static void addObject(String stringSpwanObject, World world, int spawnObjectAmount) throws Exception {
+    public static void addObject(String stringSpwanObject, World world, int spawnObjectAmount, Location spawn) throws Exception {
         for (int i = 0; i < spawnObjectAmount; i++) {
-            Location spawn = new Location(ThreadLocalRandom.current().nextInt(0, world.getSize()), ThreadLocalRandom.current().nextInt(0, world.getSize()));
-
-            while(world.containsNonBlocking(spawn) || !world.isTileEmpty(spawn)){
+            Location speceficSpawn = null;
+            if (spawn != null) {
+                speceficSpawn = spawn;
+            }
+            spawn = new Location(ThreadLocalRandom.current().nextInt(0, world.getSize()), ThreadLocalRandom.current().nextInt(0, world.getSize()));
+            while (world.containsNonBlocking(spawn) || !world.isTileEmpty(spawn)) {
                 spawn = new Location(ThreadLocalRandom.current().nextInt(0, world.getSize()), ThreadLocalRandom.current().nextInt(0, world.getSize()));
             }
-            switch(stringSpwanObject) {
+            switch (stringSpwanObject) {
                 case "rabbit":
-                    world.setTile(spawn, new Rabbit(0,5,world));
+                    world.setTile(spawn, new Rabbit(0, 5, world));
                     break;
                 case "grass":
-                    world.setTile(spawn, new Grass(0,world));
+                    world.setTile(spawn, new Grass(0, world));
                     break;
+                case "bear": {
+                    if (speceficSpawn == null){
+                        world.setTile(spawn, new Bear(0, 10, world));
+                        System.out.println("random Spawn is : " + spawn);
+                    }
+                    else{
+                        world.setTile(speceficSpawn, new Bear(0, 10, world));
+                        System.out.println("specefic Spawn is : " + speceficSpawn);
+                    }
+                    break;
+                }
                 default:
                     throw new Exception("no spawn type");
             }
