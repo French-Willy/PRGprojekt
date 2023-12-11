dd8e68f1dd1527b189c3820f7738a0a0b39eb017
ViktorEmil2000 <ViktorEMilandersen@gmail.com>
2023-12-11 18:03:26 +0100

    Med pathing

--- src/fileReader.java
+++ src/fileReader.java
@@ -7,7 +7,7 @@ public class fileReader {
     int worldsize_file;
     public ArrayList<ArrayList<String>> inputLines = new ArrayList<>();
     public void Reader() throws Exception {
-        File file = new File("data/InputFiles_UGE1/tf1-1.txt");
+        File file = new File("data/InputFiles_Uge2/t2-8a.txt");
         Scanner s = new Scanner(file);
 
         //The first line in every file is the world-size, so therefor it's easy to assign it as the first step.
